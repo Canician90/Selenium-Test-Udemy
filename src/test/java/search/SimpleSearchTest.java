58f17355c460ceeeccab24f4c6ab4e362c7ac9df
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-28 09:13:12 +0300

    SimpleSearchTest

---
+++ src/test/java/search/SimpleSearchTest.java
@@ -0,0 +1,17 @@
+package search;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleSearchTest {
+
+    @Test
+    public void simpleSearchWithOneKeyword() {
+        System.setProperty("webdriver.chrome.driver", "C:\\web drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+}
