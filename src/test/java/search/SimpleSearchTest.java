0d41854cd45362090de32c2808b0ea40b4f09283
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-28 10:32:26 +0300

    dfd

--- src/test/java/search/SimpleSearchTest.java
+++ src/test/java/search/SimpleSearchTest.java
@@ -1,11 +1,19 @@
 package search;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class SimpleSearchTest {
-    //este facut
+
 
     @Test
     public void simpleSearchWithOneKeyword() {
@@ -14,5 +22,23 @@ public class SimpleSearchTest {
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.id("search")).sendKeys("vase"+ Keys.ENTER);
+        // $x("//div[@class='product-info']//button[@title='Add to Cart']");
+        //$x("//div[@class='product-info' and ./descendant::*[text()='Herald Glass Vase']]//button[@title='Add to Cart']");
+
+        //driver.findElement(By.xpath("//div[@class='product-info' and ./descendant::*[text()='Herald Glass Vase']]//button[@title='Add to Cart']")).click();
+
+        System.out.println("Pressed Enter in search field");
+
+        List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name > a"));
+        // orice descendent fara ">"
+
+        System.out.println("Stored" + productNames.size() + "product names");
+
+        for (WebElement productName : productNames) {
+            assertThat("Some of the products name do not contain in the searched keyword",
+                    productName.getText(), containsString("VASE"));
+
+        }
     }
 }
