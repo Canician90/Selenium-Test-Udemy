b359c553258636b4aad99806e82f0199e9e6034c
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-28 09:15:41 +0300

    SimpleSearchTest

--- src/test/java/search/SimpleSearchTest.java
+++ src/test/java/search/SimpleSearchTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SimpleSearchTest {
+    //este facut
 
     @Test
     public void simpleSearchWithOneKeyword() {
